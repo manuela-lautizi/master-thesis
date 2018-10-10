85a8cf8907f6ace6d9e20b79907a57d7647323c0
Manuela Lautizi <manuelalautizi@Manuelas-MBP-7-005.homenet.telecomitalia.it>
2018-10-10 12:47:53 +0200

    Add existing file

---
+++ Script_MMAS.py
@@ -0,0 +1,149 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+@author: manuelalautizi
+"""
+import os
+os.chdir('...')
+
+import pandas as pd
+import numpy as np
+import networkx as nx
+import random
+import Module_MMAS as MMAS 
+from importlib import reload
+reload(MMAS)
+
+# =============================================================================
+# Simulated data
+# =============================================================================
+sim_net = pd.read_csv(".../netSIM.csv", index_col = 0)
+sim_gx = pd.read_csv(".../gxSIM.csv")
+
+sim_gx = sim_gx.iloc[:,1:]
+sim_gx.columns = [str(sim_gx.columns.tolist()[i]) for i in range(len(sim_gx.columns)-2)]+["os_time", "os_event"]
+sim = nx.Graph()
+
+for i in sim_net.columns.tolist():
+    for j in sim_net.columns.tolist():
+        if sim_net.loc[int(i), j] > 0.00000:
+            sim.add_edge(int(i),int(j))
+
+#Parameters
+ants = 30
+trials = 30 
+K = 4
+# Hyperparameters:
+alpha  = 1 
+beta   = 1
+rho    = 0.3   
+ph_min = 0
+ph_max = 1 
+
+# =============================================================================
+# Assignment heuristic information to edges for the computation 
+# of best theoretical solution
+# =============================================================================
+# PCA
+# =============================================================================
+net_pca = sim.copy()
+for u, v, w in net_pca.edges(data=True):        
+    pvals = [MMAS.comp_lrt_pca(sim_gx, fts) for fts in [[u], [v], [u,v]]]
+    w['weight']  = np.min(pvals[:2]) / pvals[2] 
+
+
+best_theo_pca = MMAS.best_theo(net_pca, sim_gx, K, alg = "pca")     
+greedy_pca = MMAS.greedy_algorithm(sim_gx, net_pca, K, alg = "pca")
+   
+print("Best theo (PCA)", best_theo_pca)
+print("Best greedy (PCA)", greedy_pca)
+
+# =============================================================================
+# K-Means
+# =============================================================================
+net_kmeans = sim.copy()
+for u, v, w in net_kmeans.edges(data=True):        
+    pvals = [MMAS.comp_lrt_kmeans(sim_gx, fts) for fts in [[u], [v], [u,v]]]
+    w['weight']  = np.min(pvals[:2]) / pvals[2] 
+
+best_theo_kmeans = MMAS.best_theo(net_kmeans, sim_gx, K, alg = "kmeans")     
+greedy_kmeans = MMAS.greedy_algorithm(sim_gx, net_kmeans, K, alg = "kmeans")
+   
+print("Best theo (kmeams)", best_theo_kmeans)
+print("Best greedy (kmeans)", greedy_kmeans)
+
+
+# =============================================================================
+# Application
+# =============================================================================
+pca_time = []
+kmeans_time = []
+
+pca_iter = []
+kmeans_iter = []
+
+simulations = 10
+for i in range(simulations):  
+    
+    sim_G = sim.copy()
+    print("--------PCA--------")      
+    par_pca = alpha, beta, rho, ph_min, ph_max, sim_gx, sim_G, greedy_pca[1], best_theo_pca, ants, K, trials, i
+    antRes_pca, time_pca, trials_pca, best_pca  = MMAS.MaxMin_AS_sim(par_pca, "pca")
+    
+    print("-------KMEANS-------")
+    par_kmeans = alpha, beta, rho, ph_min, ph_max, sim_gx, sim_G, greedy_pca[1], best_theo_kmeans, ants, K, trials, i
+    antRes_kmeans, time_kmeans, trials_kmeans, best_kmeans = MMAS.MaxMin_AS_sim(par_kmeans, "kmeans")
+    
+    pca_time.append(time_pca)
+    pca_iter.append(trials_pca)    
+    
+    # Jaccard
+    l = [best_pca[0][0][0]]
+    for i in range(len(best_pca[0])):
+        l.append(best_pca[0][i][1])        
+
+    print("JACCARD", MMAS.jaccard(best_theo_pca[0][0], l)*100)
+    
+
+# =============================================================================
+# Real Data
+# =============================================================================
+patients = pd.read_csv("...", ",", header = 0)
+G = pd.read_csv("...", "\t", header = None)
+
+# =============================================================================
+# Application
+# =============================================================================
+
+#Parameters
+ants = 60
+trials = 60 
+K = 10
+# Hyperparameters:
+alpha  = 1 
+beta   = 1
+rho    = 0.3   
+ph_min = 0
+ph_max = 1 
+
+simulations = 10
+for i in range(simulations):      
+    Graph = bG.copy()
+    param = alpha, beta, rho, ph_min, ph_max, patients, Gaph, ants, K, trials, i
+    antRes_pca, time_pca, trials_pca, best_pca  = MMAS.MaxMin_AS(param, "pca")
+
+
+# =============================================================================
+# If evaluation on a sample of the real data:
+# Extraction subnetwork
+# =============================================================================
+size = 500
+start = random.choice(G.nodes())
+
+nodes = MMAS.extract(G, start, size)
+subG = G.subgraph(nodes)
+nx.draw(subG)
+
+subPatients = patients[[i for i in map(str, nodes)] + ['os_time', 'os_event']]
+
+
